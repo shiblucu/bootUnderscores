93baa42b95586ab5dd7266bddfcee719fc1b5a93
Tawfiqur Rahman <tawfiqshiblu@gmail.com>
2019-01-02 15:53:23 +0600

    custom logo - add link to homepage

--- header.php
+++ header.php
@@ -45,9 +45,9 @@
                                 $custom_logo_id = get_theme_mod( 'custom_logo' );
                                 $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
                                 if ( has_custom_logo() ) {
-                                    echo '<img src="'. esc_url( $logo[0] ) .'">';
+                                    echo '<a href="'. esc_url( home_url( '/' ) ) .'"><img src="'. esc_url( $logo[0] ) .'"></a>';
                                 } else {
-                                    echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+                                    echo '<h1><a href="'. esc_url( home_url( '/' ) ) .'">'. get_bloginfo( 'name' ) .'</a></h1>';
                                 }
                                 ?>
                             </div><!-- .site-branding -->
